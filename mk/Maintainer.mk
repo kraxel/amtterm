a928ec54690ba0a5f91ecf95c502c6c7aabc5163
kraxel <kraxel>
2007-08-16 15:17:35 +0000

    makefile sync

--- mk/Maintainer.mk
+++ mk/Maintainer.mk
@@ -2,8 +2,6 @@
 ########################################################################
 
 make-sync-dir = $(HOME)/projects/gnu-makefiles
-repository = $(shell cat CVS/Repository)
-release-dir = $(HOME)/projects/Releases
 
 .PHONY: sync
 sync:: distclean
@@ -13,11 +11,18 @@ sync:: distclean
 	cp -v $(make-sync-dir)/*.mk $(srcdir)/mk
 	chmod 444 $(srcdir)/INSTALL $(srcdir)/mk/*.mk
 
+
+repository = $(shell cat CVS/Repository)
+release-dir ?= $(HOME)/projects/Releases
+release-pub ?= goldbach@me.in-berlin.de:dl.bytesex.org/releases/$(repository)
+tarball = $(release-dir)/$(repository)-$(VERSION).tar.gz
+
+.PHONY: release
 release:
 	cvs tag $(RELTAG)
 	cvs export -r $(RELTAG) -d "$(repository)-$(VERSION)" "$(repository)"
 	find "$(repository)-$(VERSION)" -name .cvsignore -exec rm -fv "{}" ";"
-	tar -c -z -f "$(release-dir)/$(repository)-$(VERSION).tar.gz" \
-		"$(repository)-$(VERSION)"
+	tar -c -z -f "$(tarball)" "$(repository)-$(VERSION)"
 	rm -rf "$(repository)-$(VERSION)"
+	scp $(tarball) $(release-pub)
 
